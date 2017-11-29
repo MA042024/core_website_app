1c98b7cd8b45a3f8ae681fc14404abe7a7f8e15d
Philippe Dessauw <philippe.dessauw@nist.gov>
2017-11-29 09:52:22 -0500

    fix: clean menus
    Change-Id: Ib0fc665964421afca99fe756e71a2ffbaf2cde0c

--- core_website_app/menus.py
+++ core_website_app/menus.py
@@ -1,7 +1,5 @@
 from django.core.urlresolvers import reverse
 from menu import Menu, MenuItem
-from core_website_app.components.account_request import api as account_request_api
-from core_website_app.components.contact_message import api as contact_message_api
 
 
 Menu.add_item(
@@ -25,19 +23,12 @@ website_children = (
     MenuItem("Privacy Policy", reverse("admin:core_website_app_privacy"), icon="user-secret"),
     MenuItem("Terms of Use", reverse("admin:core_website_app_terms"), icon="file-text-o"),
     MenuItem("Help Page", reverse("admin:core_website_app_help"), icon="question-circle-o"),
+    MenuItem("User requests", reverse("admin:core_website_app_user_requests"), icon="user-plus",
+             item_count_url="admin:core_website_app_request_count"),
     MenuItem("Contact messages", reverse("admin:core_website_app_contact_messages"), icon="envelope",
              item_count_url="admin:core_website_app_message_count"),
 )
 
-
 Menu.add_item(
-    "admin", MenuItem("WEBSITE", None, children=website_children)
+    "admin", MenuItem("WEBSITE", None, children=website_children, weight=-10000)
 )
-
-users_menu = [m for m in Menu.items["admin"] if m.title == "USERS"][0]
-users_menu_children = list(users_menu.children)
-users_menu_children.append(MenuItem("User requests", reverse("admin:core_website_app_user_requests"), icon="user-plus",
-                                    item_count_url="admin:core_website_app_request_count"))
-
-users_menu.children = users_menu_children
-
