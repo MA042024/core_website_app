752151f60f11a2180e9497a93e3d89c2877c08d8
Guillaume Sousa <guillaume.sousa@nist.gov>
2022-06-07 19:51:22 +0000

    chore: upgrade 2.0.0-beta2
    Change-Id: I2632d2b3f38daae593cd7c344af58f7e3a9d3ba2

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_website_app",
-    version="2.0.0-beta1",
+    version="2.0.0-beta2",
     description="Basic web functionalities for the curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
