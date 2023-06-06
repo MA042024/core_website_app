a49365b5acd4107e5c0104b64d119e3570d16090
Guillaume Sousa <guillaume.sousa@nist.gov>
2023-06-06 15:37:36 -0400

    fix: remove email addresses
    Change-Id: I3ad2c42afe8a8c388b741905c1e61337b517b11a

--- core_website_app/static/core_website_app/user/js/user_account_req.js
+++ core_website_app/static/core_website_app/user/js/user_account_req.js
@@ -1,17 +1,4 @@
 /**
- * 
- * File Name: user_account_req.js
- * Author: Sharief Youssef
- * 		   sharief.youssef@nist.gov
- *
- *        Guillaume SOUSA AMARAL
- *        guillaume.sousa@nist.gov
- * 
- * Sponsor: National Institute of Standards and Technology (NIST)
- * 
- */
-
-/**
  * Return to profile
  */
 returnProfile = function()
